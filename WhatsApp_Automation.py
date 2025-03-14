a1e3da9453c746819da980fe6e33f1bc87662e3e
Vaibhav Kr. Mishra <79004538+VaibhavKrMishr@users.noreply.github.com>
2025-03-14 11:57:04 +0530

    Update WhatsApp_Automation.py

--- WhatsApp_Automation.py
+++ WhatsApp_Automation.py
@@ -1,12 +1,16 @@
 import pywhatkit as kit
 import pandas as pd
 import time
+
 print("Start")
+
 # Load contacts from CSV (Modify column name based on your file)
 df = pd.read_csv("contacts.csv")
 contacts = df["Phone 1 - Value"].dropna().tolist()# Replace with correct column name
 print(df.columns)
-message = "Happy Holi🙏"
+
+message = "Hello! This is an automated message from Python."
+
 for contact in contacts:
     contact = str(contact).replace(" ", "").replace("-", "")  
     if not contact.startswith("+"):  
@@ -14,4 +18,5 @@ for contact in contacts:
     
     kit.sendwhatmsg_instantly(contact, message, wait_time=10)
     time.sleep(10)  # Delay to avoid spam detection
+    
 print("Done")
